c285619498b1a93a5d8e980c2eaf697df44cfb56
Praveen Thirumurugan <mail@praveent.com>
2025-04-04 01:05:09 +0530

    Update community-kit-form.tsx

--- src/components/community-kit-form.tsx
+++ src/components/community-kit-form.tsx
@@ -192,7 +192,7 @@ export function CommunityKitForm({
                 </div>
                 <Button type="submit" className="w-full" disabled={isLoading}>
                   {isLoading ? <Loader2 className="animate-spin" /> : null}
-                  {isLoading ? "Just a moment..." : "Submit Request"}
+                  {isLoading ? "Just a moment..." : "Generate"}
                 </Button>
               </div>
               <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
